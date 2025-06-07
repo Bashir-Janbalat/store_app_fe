02fdbe0c70f5c9f14310f7755d7543376e38808b
Bashir Janbalat <bashirjanbalat@gmail.com>
2025-06-07 15:49:06 +0200

    Refactor Layout component for improved structure and styling

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import {Outlet, useNavigate} from 'react-router-dom';
 import Header from './Header';
+import Footer from './Footer';
 import {Box} from "@mui/material";
-import Footer from "./Footer";
 import {useCart} from "../../hooks/useCart.ts";
 
 const Layout: React.FC = () => {
     const navigate = useNavigate();
     const {items} = useCart();
-    const handleCartClick = () => {
-        navigate("/cart");
-    };
 
-    const handleWishlistClick = () => {
-        navigate("/wishlist");
-    };
-
+    const handleCartClick = () => navigate("/cart");
+    const handleWishlistClick = () => navigate("/wishlist");
 
     return (
-        <>
+        <Box
+            sx={{
+                minHeight: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
             <Box dir="ltr">
-                <Header cartItemsCount={items.length}
-                        onCartClick={handleCartClick}
-                        wishlistItemsCount={2}
-                        onWishlistClick={handleWishlistClick}
+                <Header
+                    cartItemsCount={items.length}
+                    onCartClick={handleCartClick}
+                    wishlistItemsCount={2}
+                    onWishlistClick={handleWishlistClick}
                 />
             </Box>
-            <Outlet/>
+
+            <Box sx={{flex: 1}}>
+                <Outlet/>
+            </Box>
+            
             <Footer/>
-        </>
+        </Box>
     );
 };
 
